17fe378ead004d8905dcea8a68c5bfcbe7879f31
fjjosjsso <sakaraysairam1996@gmail.com>
2024-04-28 16:26:58 +0530

    Promoting code

---
+++ 08-promoting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Enter your username"
+read USERNAME
+echo "Your user name is : $USERNAME"
+echo "Provide your valiad password"
+read PASSWORD
+echo "Your secret password is : $PASSWORD"
