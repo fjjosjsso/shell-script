1da1058d404027fb34fcfb6d3eda386c80e65e39
fjjosjsso <sakaraysairam1996@gmail.com>
2024-04-28 16:30:27 +0530

    git

---
+++ 09-Promoting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Enter your username"
+read -s USERNAME
+echo "Your user name is : $USERNAME"
+echo "Provide your valiad password"
+read -s PASSWORD
+echo "Your secret password is : $PASSWORD"
