2743aa0a8ce05bad4b8a20ca21c2d3ca03fbf3ae
fjjosjsso <sakaraysairam1996@gmail.com>
2024-04-28 17:00:49 +0530

    Array  diff value

--- 02-array.sh
+++ 02-array.sh
@@ -2,4 +2,4 @@
 array=("0" "1" "2" "3" "4" "5" "6" "7" "8" "9")
 echo "The total number of numerical is : ${#array[*]}"
 echo "What numerical we have : ${array[*]}"
-echo "Get values form 2 to 7 : ${array[1:5]}"
+echo "Get values form 2 to 7 : ${array[:1:5]}"
