187484d1272b62791766a8ace910a740309273f4
fjjosjsso <sakaraysairam1996@gmail.com>
2024-04-28 17:22:50 +0530

    case condition

---
+++ 02-case_condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Type n for nginx"
+echo "Type m for mysql"
+echo "Type g for github"
+echo "Type a for appache"
+
+read choise
+
+case $choise in
+    n) yum install nginx -y ;;
+    m) yum install mysql -y ;;
+    g) yum install git -y ;;
+    a) yum install appache -y ;;
+    *) echo "please provide vaild input "
