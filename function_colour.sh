0f1a90a7261d2c7992a526a04d9f20ff707068e4
fjjosjsso <sakaraysairam1996@gmail.com>
2024-05-01 22:52:38 +0530

    fuction

---
+++ function_colour.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+R=$(\e[31m)
+G=$(\e[32m)
+P=$(\e[35m)
+N=$(\e[0m)
+validate () {
+    if [ $1 -ne 0 ]
+    then
+        echo -e"$P $2......$R Failure $N"
+    else
+        echo -e "$P $2....... $G Succues $N"
+    fi
+}   
+
+if [ $USER_ID -ne 0 ]
+    then
+        echo -e "$R Your are not in root $N"
+        exit 1
+    else
+        echo -e "$G Your are in root access $N"
+fi
+
+yum install nginx -y
+validate $2 "Installing NGINX"
+yum install mysql -y
+validate $2 "Installing MYSQL"
+yum install postfix -y
+validate $2 "Installing POSTFIX"
